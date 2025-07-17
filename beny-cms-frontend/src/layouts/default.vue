507befb400988017765ca2d58e617265fd5a670e
Даниил <danilka.volkov.02@mail.ru>
2025-07-17 22:24:34 +0300

    chore: add styles for default layout

--- beny-cms-frontend/src/layouts/default.vue
+++ beny-cms-frontend/src/layouts/default.vue
@@ -2,8 +2,24 @@
 
 <template>
   <div class="default-layout">
-    <slot></slot>
+    <div class="default-layout__wrapper">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  @use "@/assets/styles/variables" as *;
+
+  .default-layout {
+    height: 100%;
+    padding: $spacing-3;
+    background: $color-accent;
+
+    &__wrapper {
+      background: $color-whisper;
+      border-radius: $spacing-3;
+      padding: 0 $spacing-3;
+    }
+  }
+</style>
