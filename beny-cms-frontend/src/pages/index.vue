3674f5a266cc409957d8fdd84ad1f4591e42bdaa
Даниил <danilka.volkov.02@mail.ru>
2025-07-19 20:10:33 +0300

    feat: add WhyTrustUs

--- beny-cms-frontend/src/pages/index.vue
+++ beny-cms-frontend/src/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
   import { AppHeader } from "~/widgets/header";
+  import { WhyTrustUs } from "~/widgets/why-trust-us";
 </script>
 
 <template>
   <AppHeader />
+  <WhyTrustUs />
 </template>
 
 <style scoped></style>
