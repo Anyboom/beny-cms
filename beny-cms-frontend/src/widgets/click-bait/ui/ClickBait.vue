b7e093fd7a2e62c35f52b8df07144bc131305e48
Даниил <danilka.volkov.02@mail.ru>
2025-07-19 20:57:15 +0300

    fix: change div to section

--- beny-cms-frontend/src/widgets/click-bait/ui/ClickBait.vue
+++ beny-cms-frontend/src/widgets/click-bait/ui/ClickBait.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-  <div class="click-bait">
+  <section class="click-bait">
     <div class="container">
       <div class="click-bait__wrapper">
         <p class="click-bait__content">Твой успех начинается вместе с нами</p>
         <app-button href="https://vk.com" target="_blank"> Присоединиться </app-button>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
