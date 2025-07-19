0a0efc96562a91fd66966e3ca4c1249518f4fcfd
Даниил <danilka.volkov.02@mail.ru>
2025-07-19 20:43:59 +0300

    feat: add ClickBait and AppButton

--- beny-cms-frontend/src/pages/index.vue
+++ beny-cms-frontend/src/pages/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
   import { AppHeader } from "~/widgets/header";
   import { WhyTrustUs } from "~/widgets/why-trust-us";
+  import { ClickBait } from "../widgets/click-bait";
 </script>
 
 <template>
   <AppHeader />
   <WhyTrustUs />
+  <ClickBait />
 </template>
 
 <style scoped></style>
---
+++ beny-cms-frontend/src/widgets/click-bait/ui/ClickBait.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import { AppButton } from "~/shared/ui/AppButton";
+</script>
+
+<template>
+  <div class="click-bait">
+    <div class="container">
+      <div class="click-bait__wrapper">
+        <p class="click-bait__content">Твой успех начинается вместе с нами</p>
+        <app-button href="https://vk.com" target="_blank"> Присоединиться </app-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @use "@/assets/styles/variables" as *;
+
+  .click-bait {
+    margin-bottom: $spacing-8;
+    &__wrapper {
+      background: $color-default-white;
+      border-radius: $spacing-2;
+      padding: $spacing-2;
+
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    &__content {
+      color: $color-text;
+    }
+  }
+</style>
