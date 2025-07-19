bf2c5322e8f94244992e3a6be3316f32cdfc831f
Даниил <danilka.volkov.02@mail.ru>
2025-07-19 21:20:43 +0300

    feat: add OftenAsk and AppAccordion

--- beny-cms-frontend/src/pages/index.vue
+++ beny-cms-frontend/src/pages/index.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
   import { AppHeader } from "~/widgets/header";
   import { WhyTrustUs } from "~/widgets/why-trust-us";
-  import { ClickBait } from "../widgets/click-bait";
+  import { ClickBait } from "~/widgets/click-bait";
+  import { OftenAsk } from "~/widgets/ofter-ask";
 </script>
 
 <template>
   <AppHeader />
   <WhyTrustUs />
   <ClickBait />
+  <OftenAsk />
 </template>
 
 <style scoped></style>
