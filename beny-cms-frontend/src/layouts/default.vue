11ab4332ef91627d938c2c93b3b3e61c9dffd4ef
Волков Даниил <danilka.volkov.02@mail.ru>
2025-08-08 00:38:23 +0300

    chore: add responsive to DefaultLayout

--- beny-cms-frontend/src/layouts/default.vue
+++ beny-cms-frontend/src/layouts/default.vue
@@ -18,6 +18,7 @@
 
 <style scoped lang="scss">
   @use "@/assets/styles/variables" as *;
+  @use "@/assets/styles/mixins" as *;
 
   .default-layout {
     height: 100%;
@@ -29,6 +30,15 @@
       border-radius: $spacing-3;
       padding: 0 $spacing-3;
       overflow: auto;
+
+      @include view-port-sm {
+        border-radius: 0;
+        padding: 0 $spacing-1;
+      }
+    }
+
+    @include view-port-sm {
+      padding: 0;
     }
   }
 </style>
